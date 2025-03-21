021e4e653123b9a1a38477a73690f64ec3d0e9d0
nMDaas <63910563+nMDaas@users.noreply.github.com>
2025-03-20 22:04:52 -0700

    Creating an image file

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 # Image
 
-image_width = 256
-image_height = 256
+image_width = 255
+image_height = 255
 
 # Render
 
-print("P3\n" + str(image_width) + " " + str(image_height) + "\n255\n")
+with open("raytracerOutput/output.ppm", "w") as file:
+    file.write("P3\n" + str(image_width) + " " + str(image_height) + "\n255\n")
 
-for i in range(0, image_height-1):
-    for j in range(0, image_width-1):
+for i in range(0, image_height):
+    for j in range(0, image_width):
         r = i / (image_width-1)
         g = j / (image_height-1)
         b = 0.0
 
-        ir = int(255.999 * r);
-        ig = int(255.999 * g);
-        ib = int(255.999 * b);
+        ir = int(255.999 * r)
+        ig = int(255.999 * g)
+        ib = int(255.999 * b)
 
-        print(str(ir) + " " + str(ig) + " " + str(ib) + "\n")
-        
+        with open("raytracerOutput/output.ppm", "a") as file:
+            file.write(str(ir) + " " + str(ig) + " " + str(ib) + "\n")
